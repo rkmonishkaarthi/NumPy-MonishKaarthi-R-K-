7b3050f18931384cf23d7d7b79d5f4088f207cfb
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-02 21:19:16 +0530

    Avg Sales Per Month

--- Mini Project.py
+++ Mini Project.py
@@ -2,10 +2,13 @@ import numpy as np
 
 np.random.seed(42)
 
-# 100 employees, 5 months sales
+# 600 employees, 10 months sales
 sales = np.random.randint(0, 100, size=(600, 10))
-
 print("Sales Data:\n", sales)
-avg = np.mean(sales,axis=1)
 
+avg = np.mean(sales,axis=1)
 print("\nAvg Sales per Employee:\n",avg)
+
+month_avg = np.mean(sales, axis=0)
+print("\nAvg Sales per Month:\n", month_avg)
+
