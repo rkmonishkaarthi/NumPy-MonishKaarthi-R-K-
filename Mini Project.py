714bff41e90fa4cd66709807c523049905c41f47
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-02 20:57:26 +0530

    Data Range Change

--- Mini Project.py
+++ Mini Project.py
@@ -3,6 +3,6 @@ import numpy as np
 np.random.seed(42)
 
 # 100 employees, 5 months sales
-sales = np.random.randint(0, 100, size=(100, 5))
+sales = np.random.randint(0, 100, size=(600, 10))
 
 print("Sales Data:\n", sales)
