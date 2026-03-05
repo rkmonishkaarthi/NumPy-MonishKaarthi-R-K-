abc74ec0e28966082502624cf572fc7d16c0690f
rkmonishkaarthi <monishkaarthi@gmail.com>
2026-03-05 15:19:08 +0530

    fill mean

--- Pandas And NumPy/data_cleaning_manipulation.py
+++ Pandas And NumPy/data_cleaning_manipulation.py
@@ -7,3 +7,8 @@ print(df.isnull().sum())
 
 df["best_avg_score"] = df["best_avg_score"].fillna(0)
 print(df)
+
+df["review_rating"] = df["review_rating"].str.extract(r'(\d+\.?\d*)').astype(float)
+
+df["review_rating"] = df["review_rating"].fillna(df["review_rating"].mean())
+print(df)
